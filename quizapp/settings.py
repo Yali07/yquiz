9e1571ec601ff7627aa6e6b3137ae9380b77f06b
Yali07 <yaliprogrammer07@gmail.com>
2021-01-06 14:33:58 +0530

    modify settings.py

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y2*7yfa=%s%94n)-t4ycw&38=5yump9m2%(($c%l(d5=*x2ar7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yquizz.herokuapp.com']
 
 
 # Application definition
